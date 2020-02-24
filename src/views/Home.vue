100608dda2cfa502030fff06a37d01912b376f99
James Roper <james@jroper.me>
2020-02-24 13:14:52 -0700

    Make cards more semantic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,9 @@
   <div class="row">
     <div class="col-9">
       <!---->
-      <b-card>
+      <b-card tag="article">
         <img class="logo" alt="OpenUBI Project logo" src="../assets/open-ubi-project-logo.svg">
-        <b-card-text>
-          <h2>Sharing Implementation Best Practices for Universal Basic Income around the World</h2>
-        </b-card-text>
+        <h2>Sharing Implementation Best Practices for Universal Basic Income around the World</h2>
       </b-card>
     </div>
   </div>
