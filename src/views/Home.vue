bc0f28e0f87a5ad6a412f7f920999611f8cab414
James Roper <james@jroper.me>
2020-02-24 07:55:46 -0700

    Remove example component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,8 @@
 <template>
   <div class="home">
     <img class="logo" alt="OpenUBI Project logo" src="../assets/open-ubi-project-logo.svg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 .logo {
