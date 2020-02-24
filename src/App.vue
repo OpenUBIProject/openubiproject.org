919a4a6a34cd35ae7629ea4e17222d103cc5375e
James Roper <james@jroper.me>
2020-02-24 12:51:28 -0700

    Import and stylize webfonts

--- src/App.vue
+++ src/App.vue
@@ -24,16 +24,4 @@
 
 <style lang="scss">
 @import "sass/app";
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
