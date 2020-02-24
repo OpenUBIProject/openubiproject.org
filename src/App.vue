bb5f011191aeed6b53f5070b1aa886f0a127f054
James Roper <james@jroper.me>
2020-02-24 04:03:28 -0700

    Fix logo link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/open-ubi-project-logo.png">
+    <img alt="Vue logo" src="./assets/open-ubi-project-logo-full.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
