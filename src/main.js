5901794cfbe67a23d6cd4fc6fb5c73d58f063e22
James Roper <james@jroper.me>
2020-02-24 07:28:17 -0700

    Add bootstrap components library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
