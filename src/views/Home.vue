89664ae4c1cba3f53cf2aa5a4eef29daa0ba4524
James Roper <james@jroper.me>
2020-02-25 20:17:34 -0700

    Add sample cards for visual representation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="row">
-    <div class="col-9">
+    <div class="col-8">
       <b-card tag="article">
         <img class="logo" alt="OpenUBI Project logo" src="../assets/open-ubi-project-logo.svg">
         <h2>Sharing Implementation Best Practices for Universal Basic Income around the World</h2>
+      </b-card>
+    </div>
+    <div class="col-4"></div>
+    <div class="col-4">
+      <b-card
+              title="Policy Brief"
+              tag="article"
+      >
+        <b-card-text>
+          <!-- TODO -->
+          This sample description describes the implementation best practices policy brief.
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
+    <div class="col-4">
+      <b-card
+              title="Action Framework"
+              tag="article"
+      >
+        <b-card-text>
+          <!-- TODO -->
+          This sample description describes the OpenUBI Action Framework that can be used to develop specific policy for implementing UBI.
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
       </b-card>
     </div>
   </div>
