954a31c7c0b8b159b174ed0b605554856db3baa1
James Roper <james@jroper.me>
2020-02-26 04:51:31 -0700

    Rework navbar for mobile

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,26 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="primary">
-            <div class="container">
-                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-                <b-collapse id="nav-collapse" is-nav>
-                    <b-navbar-nav>
+        <div class="navbar-anchor">
+            <b-navbar toggleable="lg" type="dark" variant="primary">
+                <div class="container">
+                    <b-navbar-nav class="d-none d-lg-flex">
                         <slot></slot>
                     </b-navbar-nav>
-                </b-collapse>
 
-                <b-navbar-brand to="/">
-                    <img class="navbar-logo" src="../assets/open-ubi-project-logo-solid.svg" alt="OpenUBI Project logo">
-                </b-navbar-brand>
+                    <b-navbar-brand to="/" class="navbar-logo">
+                        <img src="../assets/open-ubi-project-logo-solid.svg" alt="OpenUBI Project logo">
+                    </b-navbar-brand>
 
-                <b-collapse id="nav-collapse" is-nav>
-                    <b-navbar-nav class="ml-auto">
+                    <b-navbar-nav class="d-none d-lg-flex">
                         <b-nav-item href="#">Twitter</b-nav-item>
                         <b-nav-item href="#">Facebook</b-nav-item>
                         <b-nav-item href="#">Discord</b-nav-item>
                         <b-nav-item href="#">Github</b-nav-item>
                     </b-navbar-nav>
-                </b-collapse>
-            </div>
-        </b-navbar>
+                </div>
+            </b-navbar>
+        </div>
+        <div class="navbar-offset mb-n4 d-lg-none"></div>
     </div>
 </template>
 
@@ -34,12 +31,42 @@
 </script>
 
 <style lang="scss">
+.navbar-anchor {
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+    top: 0;
+    height: 95px;
+}
+
+.navbar-offset {
+    height: 95px;
+}
+
 .navbar-logo {
-    max-height: 55px;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+
+    img {
+        max-height: 55px;
+    }
 }
 
 .nav-link {
     font-size: 0.9rem;
+}
+
+@media (min-width: 992px) {
+    .navbar-anchor {
+        position: initial;
+        height: initial;
+    }
+
+    .navbar-logo {
+        position: initial;
+        transform: initial;
+    }
 }
 
 @media (min-width: 1200px) {
