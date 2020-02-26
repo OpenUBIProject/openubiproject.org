0116e9360d5fc9e939006e1bf4ca285bfbb8d825
James Roper <james@jroper.me>
2020-02-25 20:20:56 -0700

    Add card breakpoints for responsive design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="row">
-    <div class="col-8">
+    <div class="col-12 col-md-8">
       <b-card tag="article">
         <img class="logo" alt="OpenUBI Project logo" src="../assets/open-ubi-project-logo.svg">
         <h2>Sharing Implementation Best Practices for Universal Basic Income around the World</h2>
       </b-card>
     </div>
     <div class="col-4"></div>
-    <div class="col-4">
+    <div class="col-12 col-md-4">
       <b-card
               title="Policy Brief"
               tag="article"
@@ -20,7 +20,7 @@
         <b-button href="#" variant="primary">Go somewhere</b-button>
       </b-card>
     </div>
-    <div class="col-4">
+    <div class="col-12 col-md-4">
       <b-card
               title="Action Framework"
               tag="article"
