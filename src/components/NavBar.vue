4ab9ec612a8437d138f1a86d5324b93df3ba4e34
James Roper <james@jroper.me>
2020-06-09 18:44:25 -0600

    Implement function sidebar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,6 +21,17 @@
             </b-navbar>
         </div>
         <div class="navbar-offset mb-n4 d-lg-none"></div>
+
+        <div class="sidebar-toggle d-lg-none" v-b-toggle.sidebar-right><span class="sidebar-toggle-icon"></span></div>
+        <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
+            <div class="px-3 py-2">
+                <p>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                    in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                </p>
+                <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+            </div>
+        </b-sidebar>
     </div>
 </template>
 
@@ -55,6 +66,57 @@
 
 .nav-link {
     font-size: 0.9rem;
+}
+
+.b-sidebar-outer {
+    position: fixed;
+    z-index: 1000;
+    width: 85%;
+    max-width: 400px;
+    top: 0;
+    height: 100%;
+}
+
+.collapsed + .b-sidebar-outer {
+    right: -401px;
+
+    transition: linear 100ms;
+}
+
+.not-collapsed + .b-sidebar-outer {
+    right: 0;
+
+    transition: linear 100ms;
+}
+
+#sidebar-right {
+    height: 100%;
+    display: block !important;
+
+    &:focus {
+        outline: none;
+    }
+}
+
+.sidebar-toggle {
+    position: fixed;
+    z-index: 110;
+    top: 14px;
+    right: 16px;
+
+    &:focus {
+        outline: none;
+    }
+}
+
+.sidebar-toggle-icon {
+    display: inline-block;
+    width: 2.5em;
+    height: 2.5em;
+    vertical-align: middle;
+    content: "";
+    background-size: 100% 100%;
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
 }
 
 @media (min-width: 992px) {
